4b8564fe7fa89e02a86c4ed25da8acada0391b1e
unknown <a5037v@gmail.com>
2019-10-11 11:46:56 -0500

    Primer commit

---
+++ src/app/app.routes.ts
@@ -0,0 +1,48 @@
+import { Routes, RouterModule } from '@angular/router';
+
+import { PagesComponent } from './pages/pages.component';
+
+import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
+import { ProgressComponent } from './pages/progress/progress.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { LoginComponent } from './login/login.component';
+import { Graficas1Component } from './pages/graficas1/graficas1.component';
+
+// Tarea 2
+import { RegisterComponent } from './login/register.component';
+
+
+
+// Se define arreglo de rutas
+const appRoutes: Routes = [
+    {   // Aquí se define la ruta raíz pero también
+        // se definen subrutas o rutas hijas
+        path: '', 
+        component: PagesComponent,
+        children: [
+            { path: 'dashboard', component: DashboardComponent },
+            { path: 'progress', component: ProgressComponent },
+            { path: 'graficas1', component: Graficas1Component },
+            { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+        ]
+    },
+    { path: 'login', component: LoginComponent },
+    { path: 'register', component: RegisterComponent },
+    /*
+    // Las siguientes rutas estaban definidas originalmente aquí
+    // pero ahora se mueven a la children de PagesComponent
+    { path: 'dashboard', component: DashboardComponent },
+    { path: 'progress', component: ProgressComponent },
+    { path: 'graficas1', component: Graficas1Component },
+    { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+    */
+
+    { path: '**', component: NopagefoundComponent }
+]
+
+// en este caso estamos también agregando el uso del hash
+// bien se pudo dejar la linea de código sin elhash pero el 
+// instructor indicó que prefiere usar hashh
+
+// LAS RUTAS SE USAN EN EL app.module.ts
+export const APP_ROUTES = RouterModule.forRoot(appRoutes, {useHash: true });

3e9c5f48ee30fc2b028f42a1d25ff3392b9896c9
unknown <a5037v@gmail.com>
2019-10-11 14:34:46 -0500

    Adición y modificación de modulos

---
+++ src/app/pages/pages.routes.ts
@@ -0,0 +1,36 @@
+import { RouterModule, Routes } from '@angular/router';
+
+import { PagesComponent } from './pages.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ProgressComponent } from './progress/progress.component';
+import { Graficas1Component } from './graficas1/graficas1.component';
+
+// Este archivo implementa las RUTAS donde se tendrán 
+// las rutas del PAGES
+// No se coloca en el app.routes para que no se complique,
+// para que no crezca tanto ese archivo y se tenga repartido
+
+
+
+const pagesRoutes: Routes = [
+
+    {   // Aquí se define la ruta raíz pero también
+        // se definen subrutas o rutas hijas
+        // NOTA. originalmente teníamos aquí estas referencias
+        // PERO SE MOVIERON a pages.routes para aligerar la
+        // cantidad de l+ineas aquí
+        path: '', 
+        component: PagesComponent,
+        children: [
+            { path: 'dashboard', component: DashboardComponent },
+            { path: 'progress', component: ProgressComponent },
+            { path: 'graficas1', component: Graficas1Component },
+            { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+        ]
+    },
+
+];
+
+// Exportamos las rutas hijas
+// Se importan en pages.module
+export const PAGES_ROUTES = RouterModule.forChild(pagesRoutes);
